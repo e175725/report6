2d288d653f7f476169dc6e6664172c42ab929f1b
Fubuki Tomori <e175725@ie.u-ryukyu.ac.jp>
2017-12-11 10:58:03 +0900

    first contact

---
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175725/Enemy.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e175725;
+
+
+public class Enemy extends LivingThing {
+
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    int HP = getHitPoint();
+    @Override
+    public void wounded(int damage){
+
+        HP -= getHitPoint();
+
+        if (HP < 0) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
